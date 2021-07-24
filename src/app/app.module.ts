b3beaab55d7e8cf135765481417b026290b66175
vladi-m1r <vsolorzano@unsa.edu.pe>
2021-07-24 18:30:40 -0500

    Se hizo pip-safe y se añadio bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { DataService } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AboutComponent } from './about/about.component';
 import { RouterModule, Route } from '@angular/router';
+import { SafePipe } from './safe.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 const routes: Route[] = [
   {path: '', component: AppComponent},
   {path: 'about', component: AboutComponent},
@@ -20,14 +22,16 @@ const routes: Route[] = [
     AppComponent,
     HelloWordComponent,
     UserComponent,
-    AboutComponent
+    AboutComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
